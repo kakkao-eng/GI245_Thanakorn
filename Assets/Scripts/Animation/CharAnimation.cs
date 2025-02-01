b98cb279ec2faa55e0747a198fdf1ca2f751dda7
kakkao71 <kakkao73@gmail.com>
2025-02-01 12:51:19 +0700

    CharactersScripts

--- Assets/Scripts/Animation/CharAnimation.cs
+++ Assets/Scripts/Animation/CharAnimation.cs
@@ -2,10 +2,33 @@ using UnityEngine;
 
 public class CharAnimation : MonoBehaviour
 {
+    private Character character;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        character = GetComponent<Character>();
+    }
+
+    private void ChooseAnimation(Character c)
+    {
+        c.Anim.SetBool("IsIdle", false);
+        c.Anim.SetBool("IsWalk", false);
+
+        switch (c.State)
+        {
+            case CharState.Idle:
+                c.Anim.SetBool("IsIdle", true);
+                break;
+            case CharState.Walk:
+                c.Anim.SetBool("IsWalk", true);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        ChooseAnimation(character);
     }
 
     // Update is called once per frame
